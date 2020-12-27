767e0bbf0d9c689e835ec489768f813ba12fed03
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2020-12-27 16:09:36 -0500

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -8,11 +8,17 @@ let
   #
   # Then, copy the resulting rev and sha256 here.
   # Last update: 2020-12-25
+  # pursPkgs = import (pkgs.fetchFromGitHub {
+  #   owner = "justinwoo";
+  #   repo = "easy-purescript-nix";
+  #   rev = "860a95cb9e1ebdf574cede2b4fcb0f66eac77242";
+  #   sha256 = "1ly3bm6i1viw6d64gi1zfiwdvjncm3963rj59320cr15na5bzjri";
+  # }) { inherit pkgs; };
   pursPkgs = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
+    owner = "ptrfrncsmrph";
     repo = "easy-purescript-nix";
-    rev = "860a95cb9e1ebdf574cede2b4fcb0f66eac77242";
-    sha256 = "1ly3bm6i1viw6d64gi1zfiwdvjncm3963rj59320cr15na5bzjri";
+    rev = "863c695b5d891a5b67ea269cd6d0e36d86662698";
+    sha256 = "1jxfcqd1b45wlsk59c9p8nsba97mbnnk2ygyv8rgcdvlkgd1fpxx";
   }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
