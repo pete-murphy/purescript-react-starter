2f2596eca4896a726dbdb2a594359674cf58fe31
Peter Murphy <26548438+pete-murphy@users.noreply.github.com>
2024-09-14 01:09:19 -0400

    feat: Simplify starter code

--- src/App.purs
+++ src/App.purs
@@ -2,7 +2,7 @@ module App where
 
 import Prelude
 
-import Data.Foldable as Foldable
+import Data.Maybe (Maybe(..))
 import Effect.Class.Console as Console
 import Effect.Timer as Timer
 import React.Basic.DOM as DOM
@@ -15,21 +15,29 @@ mkApp :: Component Unit
 mkApp = do
   Hooks.component "App" \_ -> Hooks.do
     text /\ setText <- Hooks.useState' ""
+
     timer /\ setTimer <- Hooks.useState 0
+
     Hooks.useEffect text do
       handle <- Timer.setInterval 1_000 (setTimer (_ + 1))
-      pure (setTimer (\_ -> 0) *> Timer.clearInterval handle)
+      pure do
+        setTimer (\_ -> 0)
+        Timer.clearInterval handle
+
     pure
       ( DOM.div_
           [ DOM.input
               { onChange:
                   Events.handler DOM.Events.targetValue
-                    ( Foldable.traverse_ \value -> do
+                    case _ of
+                      Just value -> do
                         Console.log value
                         setText value
-                    )
+
+                      Nothing -> mempty
+
               , value: text
               }
-          , DOM.p_ [ DOM.text (show timer) ]
+          , DOM.p_ [ DOM.text ("It's been " <> show timer <> " seconds since text was entered in the textbox") ]
           ]
       )
